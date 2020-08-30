1f0ae083c0b1ad1fc3f112b0dccbdad20ac8588c
Akki <akhilagajulapalli@Akkis-MacBook-Pro.local>
2020-08-30 23:32:11 +0530

    modes

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+let mode = document.getElementById('mode')
+let theme =  document.getElementById('theme')
 
+function changeMode(){
+    console.log(mode.checked);
+    console.log(theme);
+    mode.checked ? (
+        document.getElementById('themeName').textContent='Dark',theme.href="dark.css")
+        :(document.getElementById('themeName').textContent='Light',theme.href="light.css")
+}
+
+mode.addEventListener("click",changeMode)
 // let keyboard = document.getElementById( 'player' )
 // window.addEventListener( 'load', () => {
 //     let videos = document.querySelectorAll( '.video' )
